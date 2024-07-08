9f88c00f47bbc66ad9024bb0cf766a6dabe5941a
Fern-Aerell <fernaerell2020@gmail.com>
2024-07-08 11:45:53 +0700

    Menambahkan lagu hindia dan kangen band

--- src/pages/music/components/MusicLibrary.tsx
+++ src/pages/music/components/MusicLibrary.tsx
@@ -1,5 +1,6 @@
 import IMusic from "@/interface/IMusic";
 import MusicCard from "./MusicCard";
+import Link from "next/link";
 
 type MusicLibraryProps = {
     className?: string,
@@ -11,6 +12,14 @@ export default function MusicLibrary({className, data}: MusicLibraryProps) {
     return (
         <div className={`flex flex-col bg-[#2a2a2a] text-white p-5 gap-5 w-fit ${className}`}>
             <h1 className="font-bold text-gray-300">Music Library</h1>
+            {/* <div className="flex flex-row justify-between items-center">
+                <Link href={'/music/add'}>
+                    <svg width="25px" height="25px" viewBox="0 0 24 24" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="12" cy="12" r="10" stroke="#1C274C" stroke-width="1.5"/>
+                        <path d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                    </svg>
+                </Link>
+            </div> */}
             <div className="flex flex-col gap-3 overflow-x-hidden overflow-y-auto">
                 {
                     data ? 
